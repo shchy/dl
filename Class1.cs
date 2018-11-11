66cf0078142f87d44c0de6e916488235da5dcc61
shch <shch.pc@gmail.com>
2018-11-12 01:16:50 +0900

    'first'

---
+++ Class1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dl
+{
+    /// <summary>
+    /// 教師データ
+    /// </summary>
+    public interface ILearningData
+    {
+        /// <summary>
+        /// 入力値
+        /// </summary>
+        IEnumerable<double> Data { get; }
+        /// <summary>
+        /// 期待値
+        /// </summary>
+        IEnumerable<double> Expected { get; }
+    }
+
+    public class LearningData : ILearningData
+    {
+        public IEnumerable<double> Data { get; set; }
+
+        public IEnumerable<double> Expected { get; set; }
+
+        public static ILearningData New(IEnumerable<double> data, IEnumerable<double> expected)
+        {
+            return new LearningData
+            {
+                Data = data,
+                Expected = expected
+            };
+        }
+    }
+
+
+    /// <summary>
+    /// 学習する何か
+    /// </summary>
+    public interface IMachine
+    {
+        IEnumerable<ILayer> Layers { get; }
+
+        /// <summary>
+        /// 学習
+        /// </summary>
+        /// <param name="learningData"></param>
+        /// <returns></returns>
+        void Learn(IEnumerable<ILearningData> learningData);
+
+        /// <summary>
+        /// 判断さす
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        IEnumerable<double> Test(IEnumerable<double> data);
+    }
+
+
+    public interface ILayer
+    {
+        /// <summary>
+        /// レイヤ内のノード達
+        /// </summary>
+        IEnumerable<INode> Nodes { get; }
+    }
+
+    public class InputLayer : ILayer
+    {
+        public IEnumerable<INode> Nodes { get; set; }
+
+        public InputLayer(int inputDataSize)
+        {
+            // 固定値Nodeを作成
+            var inputNodes = Enumerable.Range(0, inputDataSize).Select(_ => new ValueNode());
+            this.Nodes = inputNodes.ToArray();
+        }
+
+        public void UpdateData(IEnumerable<double> data)
+        {
+            // todo サイズチェック
+            foreach (var item in Nodes.OfType<ValueNode>().Zip(data, Tuple.Create))
+            {
+                item.Item1.Value = item.Item2;
+            }
+        }
+    }
+
+    public class FullyConnectedLayer : ILayer
+    {
+        public IEnumerable<INode> Nodes { get; set; }
+
+        public FullyConnectedLayer(ILayer before, Func<double, double> activation, int nodeCount)
+        {
+            var nodes =
+                from i in Enumerable.Range(0, nodeCount)
+                let nodeLink = before.Nodes.MakeLink()
+                let node = new Node(activation, nodeLink)
+                select node;
+            this.Nodes = nodes.ToArray();
+        }
+    }
+
+    public static class LayerFunction
+    {
+        public static IEnumerable<INodeLink> MakeLink(this IEnumerable<INode> nodes)
+        {
+            var r = new Random(DateTime.Now.Millisecond);
+            return
+                new[] { new NodeLink { InputNode = new ValueNode{ Value = 1 }, Weight = r.NextDouble() } }
+                .Concat(nodes.Select(n => new NodeLink { InputNode = n, Weight = r.NextDouble()}))
+                .ToArray();
+        }
+    }
+
+    /// <summary>
+    /// Nodeをつなぐ線
+    /// </summary>
+    public interface INodeLink
+    {
+        /// <summary>
+        /// 重み
+        /// </summary>
+        double Weight { get; set; }
+
+        /// <summary>
+        /// 参照するNode
+        /// </summary>
+        INode InputNode { get; }
+
+    }
+
+    public class NodeLink : INodeLink
+    {
+        public double Weight { get; set; }
+
+        public INode InputNode { get; set; }
+    }
+
+    /// <summary>
+    /// Node
+    /// </summary>
+    public interface INode
+    {
+        /// <summary>
+        /// 入力Nodeのリスト
+        /// </summary>
+        IEnumerable<INodeLink> Links { get; }
+
+        /// <summary>
+        /// 活性化関数適応後の出力値
+        /// </summary>
+        double GetValue();
+
+        
+    }
+
+    public class Node : INode
+    {
+        private readonly Func<double, double> activation;
+
+        public IEnumerable<INodeLink> Links { get; set; }
+
+        public Node(Func<double, double> activation, IEnumerable<INodeLink> links)
+        {
+            this.activation = activation;
+            this.Links = links.ToArray();
+        }
+
+        public double GetValue()
+        {
+            var u = Links
+                .Select(link => link.InputNode.GetValue() * link.Weight)
+                .Sum();
+            var o = this.activation(u);
+            return o;
+        }
+    }
+
+    public class ValueNode : INode
+    {
+        public double Value { get; set; }
+
+        public IEnumerable<INodeLink> Links => Enumerable.Empty<INodeLink>();
+
+        public ValueNode()
+        {
+            this.Value = 0.0;
+        }
+
+        public double GetValue() => this.Value;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = new MyClass();
+            a.Foo();
+
+        }
+    }
+
+
+    public class MyClass
+    {
+        public void Foo()
+        {
+            // 活性化関数
+            Func<double,double> activationFunction = u => Math.Max(0, u);
+
+            // 誤差関数
+            Func<IEnumerable<Tuple<double, double>>, double> errorFunction = x => x.Sum(a => Math.Pow(a.Item1 - a.Item2, 2));
+            // 入力レイヤ
+            var inputLayer = new InputLayer(8);
+            var layer00 = new FullyConnectedLayer(inputLayer, activationFunction, 2);
+
+            var machine = new Machine(errorFunction
+                                    , inputLayer
+                                    , layer00);
+
+            var testData =
+                from x in Enumerable.Range(0, 20)
+                from y in Enumerable.Range(0, 20)
+                let isTrue = x * (y * 2) < 10
+                select LearningData.New(new double[] { x, y }, isTrue ? new[] { 1.0, 0.0 } : new[] { 0.0, 1.0 });
+            
+            machine.Learn(testData.ToArray());
+        }
+    }
+
+    
+
+    public class Machine : IMachine
+    {
+        private InputLayer firstLayer;
+        private ILayer outputLayer;
+        private readonly Func<IEnumerable<Tuple<double, double>>, double> errorFunction;
+
+        public IEnumerable<ILayer> Layers { get; set; }
+
+        public Machine(Func<IEnumerable<Tuple<double, double>>, double> errorFunction, params ILayer[] layers)
+        {
+            // todo 先頭はInputLayerであること
+            this.firstLayer = layers.First() as InputLayer;
+            this.outputLayer = layers.Last();
+            this.errorFunction = errorFunction;
+            this.Layers = layers;
+        }
+
+        public void Learn(IEnumerable<ILearningData> learningData)
+        {
+            // テストデータ分繰り返す
+            foreach (var data in learningData)
+            {
+                // 処理する
+                var result = Test(data.Data).ToArray();
+
+                // 誤差関数通す
+                var errorValue = errorFunction(result.Zip(data.Expected, Tuple.Create));
+                
+
+
+
+            }
+        }
+
+        public IEnumerable<double> Test(IEnumerable<double> data)
+        {
+            // 入力レイヤの値を更新
+            this.firstLayer.UpdateData(data);
+
+            // 出力レイヤの値を取得
+            return
+                this.outputLayer.Nodes.Select(n => n.GetValue());
+        }
+    }
+
+
+
+}
