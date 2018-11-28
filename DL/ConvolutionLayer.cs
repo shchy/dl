215e9a3fe37f6c909899c6a61d7d1f9b9eed426e
shch <shch.pc@gmail.com>
2018-11-28 12:21:14 +0900

    add stride

--- DL/ConvolutionLayer.cs
+++ DL/ConvolutionLayer.cs
@@ -12,7 +12,7 @@ namespace dl.DL
         public Action<ILayer, ILayer, Func<IEnumerable<Tuple<double, double>>, double>, ILearningData> UpdateWeightFunction { get; private set; }
         public Func<INode, double> CalcFunction { get; set; }
 
-        public ConvolutionLayer(ILayer before, int width, int chSize, int filterSize, int filterCount
+        public ConvolutionLayer(ILayer before, int width, int chSize, int filterSize, int filterCount, int stride
                                 , Func<IEnumerable<double>, IEnumerable<double>> activation
                                 , Func<double, bool> ignoreUpdate = null)
         {
@@ -20,8 +20,8 @@ namespace dl.DL
             this.ActivationFunction = activation;
             this.UpdateWeightFunction = DLF.UpdateWeight(ignoreUpdate);
             var height = (before.Nodes.Count() / width) / chSize;
-            var xSize = (width - filterSize) + 1;
-            var ySize = (height - filterSize) + 1;
+            var xSize = (int)Math.Ceiling((width - filterSize + 1) / (double)stride);
+            var ySize = (int)Math.Ceiling((height - filterSize + 1) / (double)stride);
 
             this.Nodes = (
                 from filter in Enumerable.Range(0, filterCount)
@@ -30,14 +30,11 @@ namespace dl.DL
                     from fy in Enumerable.Range(0, filterSize)
                     select Weight.Make(DLF.GetRandomWeight(), xSize * ySize * chSize))
                     .ToArray()
-                from y in Enumerable.Range(0, ySize)
-                from x in Enumerable.Range(0, xSize)
+                from y in Enumerable.Range(0, ySize * stride).Where(i => i % stride == 0)
+                from x in Enumerable.Range(0, xSize * stride).Where(i => i % stride == 0)
                 let links =
                     from channelOffset in Enumerable.Range(0, chSize)
-                    let lx = before.Nodes.MakeLink(width, filterSize, x, y + (channelOffset * height), (wx, wy) =>
-                            // Weight.Make(DLF.GetRandomWeight())
-                            weights[(wy * filterSize) + wx]
-                            ).ToArray()
+                    let lx = before.Nodes.MakeLink(width, filterSize, x, y + (channelOffset * height), (wx, wy) => weights[(wy * filterSize) + wx]).ToArray()
                     select lx
                 let node = new Node(ActivationFunction, links.SelectMany(l => l).ToArray())
                 select node).ToArray();
--- SandboxCNN.cs
+++ SandboxCNN.cs
@@ -19,12 +19,12 @@ namespace dl
             var inputLayer = new InputLayer(28 * 28);
             // 畳み込みレイヤ
             // プーリングレイヤ
-            var layer00 = new ConvolutionLayer(inputLayer, 28, 1, 3, 20, DLF.ReLU, u => u < 0);
-            var layer01 = new PoolingLayer(layer00, 26, 20, 2);
+            var layer00 = new ConvolutionLayer(inputLayer, 28, 1, 3, 8, 1, DLF.ReLU, u => u < 0);
+            var layer01 = new PoolingLayer(layer00, 26, 8, 2, 2);
             // 畳み込みレイヤ
             // プーリングレイヤ
-            var layer02 = new ConvolutionLayer(layer01, 13, 20, 3, 50, DLF.ReLU, u => u < 0);
-            var layer03 = new PoolingLayer(layer02, 11, 50, 2);
+            var layer02 = new ConvolutionLayer(layer01, 13, 8, 3, 50, 2, DLF.ReLU, u => u < 0);
+            var layer03 = new PoolingLayer(layer02, 6, 50, 2, 2);
             // 出力レイヤ
             var layer04 = new SoftmaxLayer(layer03, outputSize);
 
@@ -42,7 +42,7 @@ namespace dl
             var testData = DLF.Shuffle(new MNISTLoader().Load()).ToArray();
 
             // 0-9を均等にピックアップ
-            var pickNum = 5;
+            var pickNum = 20;
             var a = new[]{
                 testData.Take(10000).Where(x => x.Name == "0").Take(pickNum),
                 testData.Take(10000).Where(x => x.Name == "1").Take(pickNum),
@@ -54,7 +54,7 @@ namespace dl
                 testData.Take(10000).Where(x => x.Name == "7").Take(pickNum),
                 testData.Take(10000).Where(x => x.Name == "8").Take(pickNum),
                 testData.Take(10000).Where(x => x.Name == "9").Take(pickNum),
-            }.SelectMany(x=>x).ToArray();
+            }.SelectMany(x => x).ToArray();
             var b = new[]{
                 testData.Skip(10000).Where(x => x.Name == "0").Take(pickNum),
                 testData.Skip(10000).Where(x => x.Name == "1").Take(pickNum),
